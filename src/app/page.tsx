db808d75dd44d588de90cb7486a283f26aaa64cb
ThaN1eL <yuiharabusiness@gmail.com>
2025-07-23 19:25:42 +0800

    Dashboard breadcrumb, sitebar menus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import { Button } from "@/components/ui/button";
-import { DarkmodeToggle } from "@/components/common/darkmode-toggle";
-import { Input } from "@/components/ui/input";
+import { DarkmodeToggle } from '@/components/common/darkmode-toggle';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <Input />
-      <Button className="dark:bg-amber-300"> Confirm</Button>
-      <DarkmodeToggle/>
+    <div className="bg-muted flex justify-center items-center h-screen flex-col space-y-4">
+      <h1 className="text-4xl font-semibold">Welcome Nathaniel Edwin</h1>
+      <Link href="/admin">
+        <Button className="bg-teal-500 text-white">Access Dashboard</Button>
+      </Link>
     </div>
   );
 }
