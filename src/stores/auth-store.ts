518ef8ea6d20ba7c216891a62e547e0afb25ef2c
ThaN1eL <yuiharabusiness@gmail.com>
2025-08-27 21:25:28 +0800

    feat: add Zustand for state management

---
+++ src/stores/auth-store.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand';
+import { Profile } from "@/types/auth";
+import { User } from "@supabase/supabase-js";
+import { INITIAL_STATE_PROFILE } from '@/constants/auth-constant';
+
+
+type AuthState = {
+    user: User | null;
+    profile: Profile;
+    setUser: (user: User | null) => void;
+    setProfile: (profile: Profile) => void;
+};
+
+
+export const useAuthStore = create<AuthState>((set) => ({
+    user: null,
+    profile: INITIAL_STATE_PROFILE,
+    setUser: (user) => set({ user }),
+    setProfile: (profile) => set({ profile }),
+}));
