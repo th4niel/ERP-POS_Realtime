2e20e3bcb853e35352f20002e822c76fb33e03b6
ThaN1eL <yuiharabusiness@gmail.com>
2025-09-06 23:37:05 +0800

    feat: Input File Handle (WIP#1)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from "clsx"
+import { ChangeEvent } from "react"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getImageData(event: ChangeEvent<HTMLInputElement>) {}
