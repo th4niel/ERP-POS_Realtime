4d569e8771c8dff592b36774be7d61aa21ddee33
ThaN1eL <yuiharabusiness@gmail.com>
2025-09-14 21:19:52 +0800

    feat(MenuManagement): CreateMenu (WIP#2)

--- src/app/(dashboard)/admin/menu/_components/form-menu.tsx
+++ src/app/(dashboard)/admin/menu/_components/form-menu.tsx
@@ -4,13 +4,13 @@ import FormSelect from "@/components/common/form-select";
 import { Button } from "@/components/ui/button";
 import { DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Form } from "@/components/ui/form";
-import { ROLE_LIST } from "@/constants/auth-constant";
+import { AVAILABILITY_LIST, CATEGORY_LIST } from "@/constants/menu-constant";
 import { Preview } from "@/types/general";
 import { Loader2 } from "lucide-react";
 import { FormEvent } from "react";
 import { FieldValues, Path, UseFormReturn } from "react-hook-form";
 
-export default function FormUser<T extends FieldValues>({
+export default function FormMenu<T extends FieldValues>({
     form, 
     onSubmit, 
     isLoading, 
@@ -30,47 +30,56 @@ export default function FormUser<T extends FieldValues>({
         <DialogContent className="sm:max-w-[425px]">
             <Form {...form}>
                 <DialogHeader>
-                    <DialogTitle>{type} User</DialogTitle>
-                    <DialogDescription>{type=== 'Create' ? 'Register a new user' : 'Make changes user here'}</DialogDescription>
+                    <DialogTitle>{type} Menu</DialogTitle>
+                    <DialogDescription>{type=== 'Create' ? 'Add a new menu' : 'Make changes menu here'}</DialogDescription>
                 </DialogHeader>
-                <form onSubmit={onSubmit} className="space-y-4">                    
+                <form onSubmit={onSubmit} className="space-y-4">
                     <FormInput
                         form={form}
                         name={'name' as Path<T>}
                         label="Name"
-                        placeholder="Insert your name"
-                    />
-                    {type === 'Create' && (
-                        <FormInput
+                        placeholder="Insert name here"
+                    />                
+                    <FormInput
                         form={form}
-                        name={'email' as Path<T>}
-                        label="Email"
-                        placeholder="Insert email here"
-                        type="email"
-                        />
-                    )}
+                        name={'description' as Path<T>}
+                        label="Description"
+                        placeholder="Insert description here"
+                        type="textarea"
+                    />
+                    <FormSelect
+                        form={form}
+                        name={'category' as Path<T>}
+                        label="Category"
+                        selectItem={CATEGORY_LIST}
+                    />
+                    <FormInput
+                        form={form}
+                        name={'price' as Path<T>}
+                        label="Price"
+                        placeholder="Insert price here"
+                        type="number"
+                    /> 
+                    <FormInput
+                        form={form}
+                        name={'discount' as Path<T>}
+                        label="Discount"
+                        placeholder="Insert discount here"
+                        type="number"
+                    /> 
                     <FormImage 
                         form={form}
-                        name={'avatar_url' as Path<T>}
-                        label="Avatar"
+                        name={'image_url' as Path<T>}
+                        label="Image"
                         preview={preview}
                         setPreview={setPreview}
                     />
                     <FormSelect
                         form={form}
-                        name={'role' as Path<T>}
-                        label="Role"
-                        selectItem={ROLE_LIST}
+                        name={'is_available' as Path<T>}
+                        label="Availability"
+                        selectItem={AVAILABILITY_LIST}
                     />
-                    {type === 'Create' && (
-                        <FormInput
-                            form={form}
-                            name={'password' as Path<T>}
-                            label="Password"
-                            placeholder="******"
-                            type="password"
-                        />
-                    )}
                     <DialogFooter>
                         <DialogClose asChild>
                             <Button variant="outline">Cancel</Button>
--- src/constants/menu-constant.ts
+++ src/constants/menu-constant.ts
@@ -6,3 +6,30 @@ export const HEADER_TABLE_MENU = [
     'Available',
     'Action',
 ];
+
+
+export const CATEGORY_LIST = [
+  {
+  value: 'coffee',
+  label: 'Coffee',
+  },
+  {
+  value: 'patisserie',
+  label: 'Patisserie',
+  },
+  {
+  value: 'meals',
+  label: 'Meals',
+  },
+];
+
+export const AVAILABILITY_LIST = [
+  {
+  value: 'true',
+  label: 'Available',
+  },
+  {
+  value: 'false',
+  label: 'Not Available',
+  },
+];
