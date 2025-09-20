4cee524b71fa51e70550078dbda31469c39cb508
ThaN1eL <yuiharabusiness@gmail.com>
2025-09-20 18:40:21 +0800

    feat(TableManagement): implement room table list view with status indicators (WIP#1)

---
+++ src/app/(dashboard)/admin/table/page.tsx
@@ -0,0 +1,10 @@
+import TableManagement from "./_components/table";
+
+export const metadata = {
+ title: 'th4niel Cafe | Table Management',
+ 
+};
+
+export default function TableManagementPage() {
+  return <TableManagement />;
+}
