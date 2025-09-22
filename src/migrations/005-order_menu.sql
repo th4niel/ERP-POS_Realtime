6235b1a79f7df4e1af55e8fdfa1508746dbf3573
ThaN1eL <yuiharabusiness@gmail.com>
2025-09-22 19:10:21 +0800

    feat(db): creating SQL migration for order menu

---
+++ src/migrations/005-order_menu.sql
@@ -0,0 +1,14 @@
+create table public.orders_menus (
+  id serial not null,
+  order_id integer references orders on delete set null,
+  menu_id integer references menus on delete set null,
+  status text,
+  quantity integer,
+  notes text,
+  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  updated_at timestamp with time zone default timezone('utc'::text, now()) not null,
+
+  primary key (id)
+);
+
+alter table public.orders_menus enable row level security;
