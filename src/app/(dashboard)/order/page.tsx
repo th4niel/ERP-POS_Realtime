205b167f720f45a4ca6308bd72e37c4841c45566
ThaN1eL <yuiharabusiness@gmail.com>
2025-09-22 21:09:28 +0800

    feat(OrderManagement): creating order page

---
+++ src/app/(dashboard)/order/page.tsx
@@ -0,0 +1,10 @@
+import OrderManagement from "./_components/order";
+
+export const metadata = {
+ title: 'th4niel Cafe | Order Management',
+ 
+};
+
+export default function OrderManagementPage() {
+  return <OrderManagement />;
+}
