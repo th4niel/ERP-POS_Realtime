a51d0937017bd55c857edd12c0ea8b651fe42d49
ThaN1eL <yuiharabusiness@gmail.com>
2025-09-22 19:08:49 +0800

    feat(db): creating sql migration for orders

---
+++ src/migrations/004-order.sql
@@ -0,0 +1,14 @@
+create table public.orders (
+  id serial not null,
+  order_id text,
+  customer_name text,
+  status text,
+  payment_url text,
+  table_id integer references tables on delete set null,
+  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  updated_at timestamp with time zone default timezone('utc'::text, now()) not null,
+
+  primary key (id)
+);
+
+alter table public.orders enable row level security;
