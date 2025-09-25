fc0192e60ff60367c0e279caa0037f00f2877a97
ThaN1eL <yuiharabusiness@gmail.com>
2025-09-25 21:37:27 +0800

    feat(OrderManagement): Create order component for handle reservation

--- src/app/(dashboard)/order/_components/order.tsx
+++ src/app/(dashboard)/order/_components/order.tsx
@@ -8,13 +8,23 @@ import { Input } from "@/components/ui/input";
 import useDataTable from "@/hooks/use-data-table";
 import { createClient } from "@/lib/supabase/client";
 import { useQuery } from "@tanstack/react-query";
-import { Pencil, Trash2 } from "lucide-react";
-import { useMemo, useState, useCallback } from "react";
+import { Ban, Link2Icon, Pencil, ScrollText, Trash2 } from "lucide-react";
+import {
+  useMemo,
+  useState,
+  useCallback,
+  startTransition,
+  useActionState,
+  useEffect,
+} from "react";
 import { toast } from "sonner";
 import { cn } from "@/lib/utils";
 import { Table } from "@/validations/table-validation";
 import { HEADER_TABLE_ORDER } from "@/constants/order-constant";
 import DialogCreateOrder from "./dialog-create-order";
+import { updateReservation } from "../actions";
+import { INITIAL_STATE_ACTION } from "@/constants/general-constant";
+import Link from "next/link";
 
 export default function OrderManagement() {
   const supabase = createClient();
@@ -36,9 +46,12 @@ export default function OrderManagement() {
     queryFn: async () => {
       const query = supabase
         .from("orders")
-        .select(`
+        .select(
+          `
             id, order_id, customer_name, status, payment_url, tables(name, id)
-            `, { count: "exact" })
+            `,
+          { count: "exact" }
+        )
         .range((currentPage - 1) * currentLimit, currentPage * currentLimit - 1)
         .order("created_at");
 
@@ -59,16 +72,16 @@ export default function OrderManagement() {
     },
   });
 
-  const {data: tables, refetch: refetchTables} = useQuery({
-    queryKey: ['tables'],
+  const { data: tables, refetch: refetchTables } = useQuery({
+    queryKey: ["tables"],
     queryFn: async () => {
       const result = await supabase
-        .from('tables')
-        .select('*')
-        .order('created_at')
-        .order('status');
+        .from("tables")
+        .select("*")
+        .order("created_at")
+        .order("status");
 
-        return result.data;
+      return result.data;
     },
   });
 
@@ -96,13 +109,81 @@ export default function OrderManagement() {
     setTimeout(() => setSelectedTable(null), 150);
   }, []);
 
+  const totalPages = useMemo(() => {
+    return orders && orders.count !== null
+      ? Math.ceil(orders.count / currentLimit)
+      : 0;
+  }, [orders, currentLimit]);
+
+  const [reservedState, reservedAction] = useActionState(
+    updateReservation,
+    INITIAL_STATE_ACTION
+  );
+
+  const handleReservation = useCallback(async ({
+    id,
+    table_id,
+    status,
+  }: {
+    id: string;
+    table_id: string;
+    status: string;
+  }) => {
+    const formData = new FormData();
+    Object.entries({ id, table_id, status }).forEach(([Key, value]) => {
+      formData.append(Key, value);
+    });
+    startTransition(() => {
+      reservedAction(formData);
+    });
+  },[reservedAction]);
+
+  useEffect(() => {
+    if (reservedState?.status === "error") {
+      toast.error("Update Reservation Failed", {
+        description: reservedState.errors?._form?.[0],
+      });
+    }
+
+    if (reservedState?.status === "success") {
+      toast.success("Update Reservation Success");
+      refetch();
+      refetchTables();
+    }
+  }, [reservedState, refetchTables, refetch]);
+
+  const reservedActionList = useMemo(() => [
+    {
+      label: (
+        <span className="flex items-center gap-2">
+          <Link2Icon />
+          Process
+        </span>
+      ),
+      action: (id: string, table_id: string) => {
+        handleReservation({ id, table_id, status: "process" });
+      },
+    },
+    {
+      label: (
+        <span className="flex items-center gap-2">
+          <Ban className="text-red-500" />
+          Cancel
+        </span>
+      ),
+      action: (id: string, table_id: string) => {
+        handleReservation({ id, table_id, status: "canceled" });
+      },
+    },
+  ], [handleReservation]);
+
   const filteredData = useMemo(() => {
     return (orders?.data || []).map((order, index) => {
       return [
         currentLimit * (currentPage - 1) + index + 1,
         order.order_id,
         order.customer_name,
-        (order.tables as unknown as { name: string}).name,
+        (order.tables as unknown as { name: string }).name,
 
         <div
           key={`status-${order.status}`}
@@ -115,20 +196,37 @@ export default function OrderManagement() {
         >
           {order.status}
         </div>,
-        <DropdownAction key={``} menu={[]}/>,
+        <DropdownAction
+          key={``}
+          menu={
+            order.status === "reserved"
+              ? reservedActionList.map((item) => ({
+                  label: item.label,
+                  action: () =>
+                    item.action(
+                      order.id,
+                      (order.tables as unknown as { id: string }).id
+                    ),
+                }))
+              : [
+                  {
+                    label: (
+                      <Link
+                        href={`/order/${order.order_id}`}
+                        className="flex items-center gap-2"
+                      >
+                        <ScrollText />
+                        Detail
+                      </Link>
+                    ),
+                    type: "link",
+                  },
+                ]
+          }
+        />,
       ];
     });
-  }, [
-    orders?.data,
-    currentLimit,
-    currentPage,
-  ]);
-
-  const totalPages = useMemo(() => {
-    return orders && orders.count !== null
-      ? Math.ceil(orders.count / currentLimit)
-      : 0;
-  }, [orders, currentLimit]);
+  }, [orders?.data, currentLimit, currentPage, reservedActionList]);
 
   return (
     <div className="w-full">
@@ -143,7 +241,7 @@ export default function OrderManagement() {
             <DialogTrigger asChild>
               <Button variant="outline">Create</Button>
             </DialogTrigger>
-            <DialogCreateOrder tables={tables} refetch={refetch}/>
+            <DialogCreateOrder tables={tables} refetch={refetch} />
           </Dialog>
         </div>
       </div>
