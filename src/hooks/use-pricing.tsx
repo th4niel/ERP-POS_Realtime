acbbc5fed32cfdc217fcc5ec5262115a30455c6a
ThaN1eL <yuiharabusiness@gmail.com>
2025-09-27 23:09:25 +0800

    feat(OrderManagement): update hooks usePricing  to null undefined

--- src/hooks/use-pricing.tsx
+++ src/hooks/use-pricing.tsx
@@ -1,10 +1,10 @@
 import { Menu } from "@/validations/menu-validation";
 import { useMemo } from "react";
 
-export function usePricing(orderMenu: {menus: Menu; quantity: number}[]) {
+export function usePricing(orderMenu: {menus: Menu; quantity: number}[] | null | undefined) {
     const totalPrice = useMemo(() => {
         let total = 0;
-        orderMenu.forEach((item) => {
+        orderMenu?.forEach((item) => {
             total += item.menus.price * item.quantity;
         });
         return total;
