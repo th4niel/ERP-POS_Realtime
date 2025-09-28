8645cdde561b89941efea384bfec3b6b67296142
ThaN1eL <yuiharabusiness@gmail.com>
2025-09-28 23:09:18 +0800

    feat(OrderManagement): creating components add order item for show menu page

---
+++ src/app/(dashboard)/order/[id]/add/_components/add-order-item.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { FILTER_MENU } from "@/constants/order-constant";
+import useDataTable from "@/hooks/use-data-table";
+import { createClient } from "@/lib/supabase/client";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+export default function AddOrderItem({ id }: { id: string }) {
+const supabase = createClient();
+  const {
+    currentPage,
+    currentSearch,
+    currentFilter,
+    handleChangePage,
+    handleChangeSearch,
+    handleChangeFilter,
+  } = useDataTable();
+
+  const {data: menus,isLoading: isLoadingMenu} = useQuery({
+    queryKey: ["menus", currentPage, currentSearch],
+    queryFn: async () => {
+      const query = supabase
+        .from("menus")
+        .select("*", { count: "exact" })
+        .order("created_at")
+        .eq('is_available', true)
+        .ilike('name', `%${currentSearch}%`);
+
+      if (currentFilter) {
+        query.eq('category', currentFilter)
+      }
+
+      const result = await query;
+
+      if (result.error)
+        toast.error("Get Menu data failed", {
+          description: result.error.message,
+        });
+
+      return result;
+    },
+  });
+
+  const { data: order } = useQuery({
+    queryKey: ["order", id],
+    queryFn: async () => {
+      const result = await supabase
+        .from("orders")
+        .select("id, customer_name, status, payment_url, tables(name, id)")
+        .eq("order_id", id)
+        .single();
+
+      if (result.error)
+        toast.error("Get Order data failed", {
+          description: result.error.message,
+        });
+
+      return result.data;
+    },
+    enabled: !!id,
+  });
+
+  return (
+    <div className="flex flex-col lg:flex-row gap-4 w-full">
+      <div className="space-y-4 lg:w-2/3">
+        <div className="flex flex-col items-center justify-between gap-4 w-full lg:flex-row">
+            <div className="flex flex-col lg:flex-row items-center gap-4">
+                <h1 className="text-2xl font-bold">Menu</h1>
+                <div className="flex  gap-2">
+                    {FILTER_MENU.map((item) => (
+                        <Button 
+                            key={item.value}
+                            onClick={() => handleChangeFilter(item.value)}
+                            variant={currentFilter === item.value ? 'default': 'outline'}
+                        >
+                            {item.label}
+                        </Button>
+                    ))}
+                </div>
+            </div>
+            <Input placeholder="Search..." onChange={(e) => handleChangeSearch(e.target.value)}/>
+        </div>
+      </div>
+      <div className="lg:w-1/3"></div>
+    </div>
+  );
+}
