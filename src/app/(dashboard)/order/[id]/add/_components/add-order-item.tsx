a83ad5eb4653e88a8a5833520f061645967f348a
ThaN1eL <yuiharabusiness@gmail.com>
2025-09-29 15:36:33 +0800

    feat(OrderManagement): add to cart logic

--- src/app/(dashboard)/order/[id]/add/_components/add-order-item.tsx
+++ src/app/(dashboard)/order/[id]/add/_components/add-order-item.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -10,9 +10,12 @@ import { toast } from "sonner";
 import CardMenu from "./card-menu";
 import LoadingCardMenu from "./loading-card-menu";
 import CartSection from "./cart";
+import { useState } from "react";
+import { Cart } from "@/types/order";
+import { Menu } from "@/validations/menu-validation";
 
 export default function AddOrderItem({ id }: { id: string }) {
-const supabase = createClient();
+  const supabase = createClient();
   const {
     currentSearch,
     currentFilter,
@@ -20,18 +23,18 @@ const supabase = createClient();
     handleChangeFilter,
   } = useDataTable();
 
-  const {data: menus,isLoading: isLoadingMenu} = useQuery({
+  const { data: menus, isLoading: isLoadingMenu } = useQuery({
     queryKey: ["menus", currentSearch, currentFilter],
     queryFn: async () => {
       const query = supabase
         .from("menus")
         .select("*", { count: "exact" })
         .order("created_at")
-        .eq('is_available', true)
-        .ilike('name', `%${currentSearch}%`);
+        .eq("is_available", true)
+        .ilike("name", `%${currentSearch}%`);
 
       if (currentFilter) {
-        query.eq('category', currentFilter)
+        query.eq("category", currentFilter);
       }
 
       const result = await query;
@@ -64,32 +67,74 @@ const supabase = createClient();
     enabled: !!id,
   });
 
+  const [carts, setCarts] = useState<Cart[]>([]);
+  const handleAddtoCart = (menu: Menu, action: "increment" | "decrement") => {
+    const existingItem = carts.find((item) => item.menu_id === menu.id);
+    if (existingItem) {
+      if (action === "decrement") {
+        if (existingItem.quantity > 1) {
+          setCarts(
+            carts.map((item) =>
+              item.menu_id === menu.id
+                ? {
+                    ...item,
+                    quantity: item.quantity - 1,
+                    total: item.total - menu.price,
+                  }
+                : item,
+            ),
+          );
+        } else {
+          setCarts(carts.filter((item) => item.menu_id !== menu.id));
+        }
+      } else {
+        setCarts(
+          carts.map((item) =>
+            item.menu_id === menu.id
+              ? {
+                  ...item,
+                  quantity: item.quantity + 1,
+                  total: item.total + menu.price,
+                }
+              : item,
+          ),
+        );
+      }
+    } else {
+      setCarts([...carts, {menu_id: menu.id, quantity: 1, total: menu.price, notes: '', menu},      
+      ]);
+    }
+  };
+
   return (
     <div className="flex flex-col lg:flex-row gap-4 w-full">
       <div className="space-y-4 lg:w-2/3">
         <div className="flex flex-col items-center justify-between gap-4 w-full lg:flex-row">
-            <div className="flex flex-col lg:flex-row items-center gap-4">
-                <h1 className="text-2xl font-bold">Menu</h1>
-                <div className="flex  gap-2">
-                    {FILTER_MENU.map((item) => (
-                        <Button 
-                            key={item.value}
-                            onClick={() => handleChangeFilter(item.value)}
-                            variant={currentFilter === item.value ? 'default': 'outline'}
-                        >
-                            {item.label}
-                        </Button>
-                    ))}
-                </div>
+          <div className="flex flex-col lg:flex-row items-center gap-4">
+            <h1 className="text-2xl font-bold">Menu</h1>
+            <div className="flex  gap-2">
+              {FILTER_MENU.map((item) => (
+                <Button
+                  key={item.value}
+                  onClick={() => handleChangeFilter(item.value)}
+                  variant={currentFilter === item.value ? "default" : "outline"}
+                >
+                  {item.label}
+                </Button>
+              ))}
             </div>
-            <Input placeholder="Search..." onChange={(e) => handleChangeSearch(e.target.value)}/>
+          </div>
+          <Input
+            placeholder="Search..."
+            onChange={(e) => handleChangeSearch(e.target.value)}
+          />
         </div>
         {isLoadingMenu && !menus ? (
           <LoadingCardMenu />
         ) : (
           <div className="grid grid-cols-2 lg:grid-cols-3 w-full gap-4">
             {menus?.data?.map((menu) => (
-              <CardMenu menu={menu} key={`menu-${menu.id}`}/>
+              <CardMenu menu={menu} key={`menu-${menu.id}`} onAddToCart={handleAddtoCart}/>
             ))}
           </div>
         )}
@@ -98,7 +143,12 @@ const supabase = createClient();
         )}
       </div>
       <div className="lg:w-1/3">
-        <CartSection order={order}/>
+        <CartSection 
+          order={order} 
+          carts={carts} 
+          setCarts={setCarts} 
+          onAddToCart={handleAddtoCart}
+        />
       </div>
     </div>
   );
