6881dd36dc2b326cd00cd5ad800d6473346c1b5d
ThaN1eL <yuiharabusiness@gmail.com>
2025-10-01 23:02:25 +0800

    fix: revert back to createClientSupabase

--- src/app/(dashboard)/order/[id]/_components/detail-order.tsx
+++ src/app/(dashboard)/order/[id]/_components/detail-order.tsx
@@ -4,7 +4,7 @@ import DataTable from "@/components/common/data-table";
 import { Button } from "@/components/ui/button";
 import { HEADER_TABLE_DETAIL_ORDER } from "@/constants/order-constant";
 import useDataTable from "@/hooks/use-data-table";
-import { createClient} from "@/lib/supabase/client";
+import { createClientSupabase} from "@/lib/supabase/default";
 import { cn, convertUSD } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
@@ -24,7 +24,7 @@ import { INITIAL_STATE_ACTION } from "@/constants/general-constant";
 import { useAuthStore } from "@/stores/auth-store";
 
 export default function DetailOrder({ id }: { id: string }) {
-  const supabase = createClient();
+  const supabase = createClientSupabase();
   const { currentPage, currentLimit, handleChangePage, handleChangeLimit } =
     useDataTable();
   const profile = useAuthStore((state) => state.profile);
@@ -123,6 +123,7 @@ export default function DetailOrder({ id }: { id: string }) {
 
     if (updateStatusOrderState?.status === "success") {
       toast.success("Update Status Order Success");
+      refetchOrderMenu();
     }
   }, [updateStatusOrderState, refetchOrderMenu]);
 
