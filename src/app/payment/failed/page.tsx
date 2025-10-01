162f80b759c1990c9d589c2da2edbc2e712d0d6e
ThaN1eL <yuiharabusiness@gmail.com>
2025-10-01 19:28:38 +0800

    feat(PaymentGateaway): page for handle failed

---
+++ src/app/payment/failed/page.tsx
@@ -0,0 +1,10 @@
+import Failed from "./_components/failed";
+
+export const metadata = {
+ title: 'th4niel Cafe | Payment Failed',
+ 
+};
+
+export default function FailedPage() {
+  return <Failed />;
+}
