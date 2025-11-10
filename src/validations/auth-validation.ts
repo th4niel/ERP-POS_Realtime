ee9b05404e62d5e44debc6105ed2bbffbd4b4551
ThaN1eL <yuiharabusiness@gmail.com>
2025-11-10 23:10:57 +0800

    fix: add regex for the password validation

--- src/validations/auth-validation.ts
+++ src/validations/auth-validation.ts
@@ -13,7 +13,12 @@ export const createUserSchema = z.object({
     .string()
     .min(1, 'Email is required')
     .email('Please enter a valid email'),
-  password: z.string().min(1, 'Password is required'),
+  password: z.string()
+      .min(8, "Password must be at least 8 characters")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(/[^A-Za-z0-9]/,"Password must contain at least one special character"),
   name: z.string().min(1, 'Name is required'),
   role: z.string().min(1, 'Role is required'),
   avatar_url: z.union([z.string().min(1, 'Image URL is required'), z.instanceof(File),]),
